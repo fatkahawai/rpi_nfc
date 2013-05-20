fddc706c67bf7ff5a6f334573acdbad56f5e9e81
fatkahawai <ceeb@burning-plains.com>
2013-05-20 22:22:47 +1200

    sh fix

--- compile_rpi_nfc.sh
+++ compile_rpi_nfc.sh
@@ -1,3 +1,3 @@
-echo gcc -o rpi_nfc rpi_nfc.c tcp_client.c nfc_driver.c led_driver.c nfc-utils.c -lnfc 
+echo gcc -o rpi_nfc rpi_nfc.c tcp_client.c nfc_driver.c led_driver.c nfc-utils.c -lnfc ../wiringPi/wiringPi/libwiringPi.so.2.0
 
-gcc -o rpi_nfc rpi_nfc.c tcp_client.c nfc_driver.c led_driver.c nfc-utils.c -lnfc 
+gcc -o rpi_nfc rpi_nfc.c tcp_client.c nfc_driver.c led_driver.c nfc-utils.c -lnfc ../wiringPi/wiringPi/libwiringPi.so.2.0
